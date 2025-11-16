b4528a8ca1b35c353c7ab9d6c27d3807aa6c8cc1
Nha Uyen <23521765@gm.uit.edu.vn>
2025-11-16 16:42:09 +0700

    Update code in file

--- src/components/ResortSection/components/ResortAddress.tsx
+++ src/components/ResortSection/components/ResortAddress.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { MapPin } from "lucide-react";
 
-function ResortAddress() {
-  return (
-    <div>
-      ResortAddress
-    </div>
-  )
+interface ResortAddressProps {
+  address: string;
 }
+function ResortAddress({address}: ResortAddressProps) {
+  return (
+    <div className="flex items-center gap-1 text-gray-600 text-[13px] mt-1 hover:text-[#0071c2]">
+      <MapPin size={14} />
+      <span>{address}</span>
+    </div>
+  );
+};
 
 export default ResortAddress
--- src/components/ResortSection/components/ResortName.tsx
+++ src/components/ResortSection/components/ResortName.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function ResortName() {
+interface ResortNameProps {
+  name: string;
+}
+
+function ResortName({name}: ResortNameProps) {
+ 
   return (
-    <div>
-      ResortName
-    </div>
-  )
+    <h3 className="text-[18px] font-semibold text-gray-800 hover:text-[#0071c2] cursor-pointer">
+      {name}
+    </h3>
+  );
 }
 
 export default ResortName
