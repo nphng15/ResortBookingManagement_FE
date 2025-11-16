1333d954b5787769729d793a310680c6f809b36b
Nha Uyen <23521765@gm.uit.edu.vn>
2025-11-16 16:55:05 +0700

    fe

--- src/components/ResortSection/ResortCard/ServiceTag.tsx
+++ src/components/ResortSection/ResortCard/ServiceTag.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function ServiceTag() {
+interface ServiceTagProps {
+  label: string;
+  color?: string;
+}
+
+function ServiceTag({label, color}: ServiceTagProps) {
+  const bg =
+    color ||
+    (label.toLowerCase().includes("free")
+      ? "bg-green-100 text-green-700"
+      : "bg-blue-100 text-blue-700");
   return (
-    <div>
-      ServiceTag
+    <div className="flex gap-2">
+      <span className={`text-xs font-medium px-2 py-[2px] rounded-md ${bg}`}>
+      {label}
+    </span>
     </div>
   )
 }
 
-export default ServiceTag
+export default ServiceTag;
